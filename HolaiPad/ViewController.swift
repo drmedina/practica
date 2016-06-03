2f76447d1b1288bb1517b428a23dfe4fb71e2a6f
Manuel Medina <dr_medina@outlook.com>
2016-06-02 20:49:32 -0400

    Probando cambios. Probando repo

--- HolaiPad/ViewController.swift
+++ HolaiPad/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func nombre(sender: AnyObject) {
+        result.text = "Manuel"
+    }
+    
+    @IBAction func Apellido(sender: AnyObject) {
+        result.text = "Medina"
+        view.endEditing(true)
+    }
+    
+    @IBAction func Ciudad(sender: AnyObject) {
+        result.text = "Santo Domingo"
+        view.endEditing(true)
+    }
+    
+    @IBAction func Pais(sender: AnyObject) {
+        result.text = "Rep. Dom."
+        view.endEditing(true)
+    }
+// Probando!!!
+    
 }
 
